3c62d08f2b2a4c6f15bd09a4459ee1039c6892f5
lilimo70 <lilimo70@gmail.com>
2026-02-25 11:49:56 +0900

    3分岐の条件分岐（else if）をマスター。接骨院の年齢別料金ロジック実装完了！

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-// 【課題】会員ランク別・ポイント付与
-// ルール：
-// 1. 購入金額（purchaseAmount）を受け取る。
-// 2. もし「VIP会員（isVIP）」が true なら、購入金額の 10% をポイントにする。
-// 3. もし「VIP会員ではない（false）」なら、購入金額の 1% をポイントにする。
-// 4. 最後に、計算した「付与ポイント（point）」を返す。
+//関数名 calcFaresByAge を定義する。
 
-// 引数1: purchaseAmount (購入金額)
-// 引数2: isVIP (VIPならtrue)
-
-// --- ここから下に、1から書いてみましょう！ ---
-
-const calculatePoints = (purchaseAmount,isVIP) => {
-    if (isVIP === true){
-        return purchaseAmount * 0.1;
+const calcFaresByAge = (age) => {
+    if (age < 18) {
+        return 1000;
+    }
+    else if (age >= 65) {
+        return 2000;
     }
     else {
-        return purchaseAmount * 0.01;
+        return 5000;
     }
 }
-
-// --- 動作確認（購入金額 10,000円、VIP会員の場合） ---
-// const getPoint = 関数名(10000, true);
-const getPoint = calculatePoints(10000,true);
-// console.log(`付与ポイントは${getPoint}ptです`);
-console.log(`付与ポイントは${getPoint}ptです`);
+const total = calcFaresByAge(18);
+console.log(`料金は${total}です`);
