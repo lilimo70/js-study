bb8ad583ece50d2772eb9c515ee39b2f36bce2a1
lilimo70 <lilimo70@gmail.com>
2026-02-27 14:03:01 +0900

    接骨院クラス設計
    親元の設計図（クラス）から個別の個性を持つ患者（インスタンス）を生成。Constructorによる値の紐付けとメソッドの基礎を習得し、実務フローを完遂。

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-// 【課題】カフェのサイズ別料金計算システム
-// 
-// 関数名：calcDrinkPrice
-// 引数：size (文字列で "S" か "M" か "L" が入ってくる想定)
-//
-// ルール：
-// 1. もし size が "S" なら、料金 300 を返す。
-// 2. もし size が "M" なら、料金 450 を返す。
-// 3. それ以外（"L"）なら、料金 600 を返す。
-//
-// 最後に、好きなサイズを引数に入れて実行し、
-// コンソールに「価格は〇〇円です」と表示させてください。
-
-// --- ここから下に書いてみましょう！ ---
-const calcDrinkPrice = (size) => {
-    if (size === "S"){
-        return 300;
-    }
-    else if (size === "M"){
-        return 450;
-    }
-    else {
-        return 600;
+class Patient {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
 }
-const totalCost = calcDrinkPrice("L")
-console.log(`料金は${totalCost}です`);
+const patient1 = new Patient("佐藤さん", 30);
+console.log(`名前: ${patient1.name}, 年齢: ${patient1.age}`);
