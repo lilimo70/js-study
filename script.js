6ba02e49f672dc7331378c912ebd46550adc5610
lilimo70 <lilimo70@gmail.com>
2026-03-02 11:20:49 +0900

    患者の年齢と来院日数による判定・料金計算機能の実装
    年齢条件を追加。将来の税率変更やキャンペーン変更に強い構造にするためクラスのメソッドで定義

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 class Reservation {
-    constructor(patientName, daysSinceLastVisit) {
+    constructor(patientName, daysSinceLastVisit, age) {
         this.patientName = patientName;
         this.daysSinceLastVisit = daysSinceLastVisit;
+        this.age = age;
     }
     judgeStatus() {
         if (this.daysSinceLastVisit >= 14) {
@@ -10,13 +11,21 @@ class Reservation {
             return "順調です。3日後に経過を見せに来てください";
         }
     }
+    judgeAge() {
+        if (this.age < 18) {
+            return 800;
+        } else {
+            return 1000;
+        }
+    }
+
     printMessage() {
-        console.log(`${this.patientName}様、${this.judgeStatus()}`);
+        console.log(`${this.patientName}様、${this.judgeStatus()}。料金は${this.judgeAge()}円です。`);
     }
 }
 
-const patient1 = new Reservation("佐藤さん", 30);
+const patient1 = new Reservation("佐藤さん", 30, 10);
 patient1.printMessage();
 
-const patient2 = new Reservation("鈴木さん", 80);
+const patient2 = new Reservation("鈴木さん", 80, 20);
 patient2.printMessage();
